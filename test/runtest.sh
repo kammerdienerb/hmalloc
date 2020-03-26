d10d328ada6cff2de605e4f4c64382cbada84fc5
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-03-26 10:50:42 -0400

    dont want those any more

--- test/runtest.sh
+++
@@ -1,3 +0,0 @@
-# DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES="/Users/kammerdienerb/Documents/GitHub/hmalloc/lib/libhmalloc.dylib" DYLD_LIBRARY_PATH="/Users/kammerdienerb/Documents/GitHub/hmalloc/lib" ./$1
-# DYLD_LIBRARY_PATH="/Users/kammerdienerb/Documents/GitHub/hmalloc/lib/" ./$1
-LD_PRELOAD="/home/bkammerd/hmalloc/lib/libhmalloc.so" ./$1
--- test/test_pp.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <iostream>
-
-std::string s  = "this string should be too long for SSA -- I'll add some more just to be sure. ";
-int main() {
-    std::string s2 = s;
-    std::cout << s2 + s << "\n";
-}
